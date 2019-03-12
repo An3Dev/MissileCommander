42c04445068cc456893ad0acb075c874287bc61a
An3Dev <an3Developer@gmail.com>
2019-03-12 04:53:54 -0700

    Made moving platform

---
+++ MissileCommander/Assets/Scripts/MovingBarrier.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBarrier : MonoBehaviour {
+
+    public bool movingRight = true;
+    
+    public float speed;
+    Rigidbody2D rb;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (movingRight)
+        {
+            rb.velocity = rb.transform.right * speed;
+            //Debug.Log("Right");
+        }
+
+        if (!movingRight)
+        {
+            rb.velocity = -rb.transform.right * speed;
+            //Debug.Log("Left");
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        
+        if (collision.collider.tag == "DestroyMissile")
+        {
+            switch(movingRight)
+            {
+                case false:
+                    movingRight = true;
+                    break;
+
+                case true:
+                    movingRight = false;
+                    break;
+
+                default:
+                    movingRight = true;
+                    break;
+            }
+            
+        }
+
+        
+    }
+}
